dfa03c575babc84938f5e75f152fce87a2220b47
Colton Hammond <HammondColton@outlook.com>
2023-11-07 14:37:43 -0600

    Finshed adding Views

--- PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace PersonalSiteMVC.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
